dec16be9de2168c815d0acb86c3ca018c4df8114
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2020-11-26 01:10:49 +0100

    Add face-fader

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -25,8 +25,25 @@ function useScrollSpy() {
   });
 }
 
+function useFaceFader() {
+  const $faceFader = $('.face-fader');
+  const $faceWithBeard = $('.face-with-beard');
+  let previousOpacity = undefined;
+
+  window.onscroll = function() {
+    const scrollPosition = window.scrollY;
+    const startOffset = $faceFader.offset().top + $faceFader.height() - window.innerHeight;
+    const range = window.innerHeight - $faceFader.height();
+    const opacity = Math.min(1.0, Math.max(0.0, Math.round((scrollPosition - startOffset) / range * 10) / 10));
+    if (opacity !== previousOpacity) {
+      $faceWithBeard.css('opacity', previousOpacity = opacity);
+    }
+  }
+}
+
 window.addEventListener('load', function() {
   useNavbarCollapsePixelObserver();
   useScrollToBehavior();
   useScrollSpy();
+  useFaceFader();
 });
